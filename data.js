3c029ae068b76a67261e3012aea1d351cd179354
Eric Wegscheid <ericwegscheid@mailfence.com>
2023-04-27 16:05:56 -0500

    -

--- data.js
+++ data.js
@@ -2,6 +2,9 @@ window._data = {
   "currentSet": [],
   "charts": [
     "./charts/Clearly.ccl",
-    "./charts/Rain.ccl"
+    "./charts/Rain.ccl",
+    "./charts/DustInTheWind.ccl",
+    "./charts/TheSoundOfSilence.ccl",
+    "./charts/WhereHaveAllTheCowboysGone.ccl",
   ]
 };
