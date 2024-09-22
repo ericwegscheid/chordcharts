057d1f3d8914981f353d3ec91a3e800b24c3caed
Eric Wegscheid <ericwegscheid@mailfence.com>
2024-09-21 22:39:33 -0500

    add toggle view

--- js/action.js
+++ js/action.js
@@ -2,10 +2,10 @@
 
   window._action = {
     clearFilter: function() {
-        _data.filter = '';
-        __.select('.menu .search input').value = '';
-        __.select('.menu .search .clear-filter').classList.toggle('show', !!_data.filter);
-        _render.menu();
+      _data.filter = '';
+      __.select('.menu .search input').value = '';
+      __.select('.menu .search .clear-filter').classList.toggle('show', !!_data.filter);
+      _render.menu();
     },
     filterMenu: function(e) {
       if( e.key.match(/[a-z0-9\s]+/ig) ) {
@@ -21,14 +21,12 @@
         '?queue=',
         _data.queue.map(function(chartIndex) {
           var chart = _data.charts[chartIndex];
-
           return [
             chart.id,
             chart.transposition ? ':' + chart.transposition : ''
           ].join('');
         }).join(',')
       ].join('');
-
       __.copy(str);
       alert('Copied to clipboard: ' + str);
     },
@@ -57,12 +55,10 @@
     },
     removeFromQueue: function(index) {
       var chart = _data.charts[_data.queue[index]];
-
       if( chart.id === _data.selectedChart.id ) {
         __.select('h2').innerHTML = '';
         _render.chart();
       }
-
       _data.queue.splice(index, 1);
       _app.saveData('queue');
       _render.queue();
@@ -70,11 +66,9 @@
     },
     addToQueue: function(index) {
       var chart = _data.charts[index];
-
       if( !chart || !!~_data.queue.indexOf(chart.index)) {
         return;
       }
-
       _data.queue.push(index);
       _app.saveData('queue');
       _render.queue();
@@ -83,61 +77,45 @@
     showChart: function(chart) {
       _proc.chart(chart);
       _render.chart(chart);
-
       var active = __.select('.menu ul li.active');
-
       if( active ) {
         active.classList.remove('active');
       }
-
       var listItem = __.select('#chart-' + chart.id);
-
       if( listItem ) {
         listItem.classList.add('active');
       }
-
       __.select('h2').innerHTML = chart.title;
-
       document.body.classList.remove('no-chart');
     },
     selectChartFromQueue: function(index, willRender) {
       var chart = _data.charts[_data.queue[index]];
-
       if( !chart ) {
         return;
       }
-
       _data.queueIndex = index;
       _app.saveData('queueIndex');
-
       _action.selectChart(_data.charts[_data.queue[index]].index);
     },
     selectChart: function(index) {
       var chart = _data.charts[index];
-
       if( !chart ) {
         console.warn('No chart selected');
         _render.chart();
-
         return;
       }
-
       if( (_data.selectedChart || {}).id === chart.id ) {
         _data.charts[chart.index] = _data.selectedChart;
 //         _app.saveData('charts');
         _action.showChart(_data.selectedChart);
-
         return;
       }
-
       _data.selectedChart = chart;
       _app.saveData('selectedChart');
-
       __.get(
         chart.src,
         function(req) {
           chart.text = req.response;
-
           _action.showChart(chart);
         },
         function(req) {
@@ -145,9 +123,8 @@
         }
       );
     },
-    collapse: function() {
-      var isFull = document.body.classList.contains('full');
-
+    collapse: function(override) {
+      var isFull = typeof override === 'boolean' ? !override : document.body.classList.contains('full');
       document.body.classList.toggle('full', !isFull);
       __.select('.collapse').innerHTML = isFull ? '&lsaquo;' : '&rsaquo;'
     },
@@ -164,10 +141,8 @@
             ? !document.body.classList.contains('dark')
             : force,
         theme = dark ? 'LIGHT' : 'DARK';
-
       document.body.classList.toggle('dark', dark);
       __.select('span.theme button').innerHTML = theme;
-
       // save theme
       _data.useDarkTheme = dark ? 1 : 0;
       _app.saveData('useDarkTheme');
@@ -176,24 +151,19 @@
       var hideChords = typeof force !== 'boolean'
             ? !_data.selectedChart.hideChords
             : force;
-
       _data.selectedChart.hideChords = hideChords;
-
       _app.saveData('selectedChart');
       _render.chart(_data.selectedChart, true);
-
       document.body.classList.toggle('lyrics-only', hideChords);
       __.select('span.lyrics button').innerHTML = hideChords ? 'CHORDS' : 'LYRICS';
     },
     columns: function(force) {
       var chart = __.select('.chart'),
-          columns = typeof force !== 'boolean'
-            ? chart.classList.contains('no-columns')
-            : force;
-
+        columns = typeof force !== 'boolean'
+          ? chart.classList.contains('no-columns')
+          : force;
       _data.selectedChart.columns = columns;
       _app.saveData('selectedChart');
-
       chart.classList.toggle('no-columns', !columns);
       __.select('button.columns').innerHTML = columns ? 'ON' : 'OFF';
     },
@@ -202,10 +172,8 @@
           floatLines = typeof force !== 'boolean'
             ? !chart.classList.contains('float-lines')
             : force;
-
       _data.selectedChart.floatLines = floatLines;
       _app.saveData('selectedChart');
-
       chart.classList.toggle('float-lines', floatLines);
       __.select('button.float-lines').innerHTML = floatLines ? 'ON' : 'OFF';
     },
@@ -242,10 +210,13 @@
     transpose: function(direction) {
       _data.selectedChart.transposition += direction;
       _app.saveData('selectedChart');
-
       _proc.chart(_data.selectedChart);
       _render.chart(_data.selectedChart);
-    }
+    },
+    toggleControls: function() {
+      __.select('body').classList.toggle('hide-controls');
+      _action.collapse(true);
+    },
   };
 
 })();
